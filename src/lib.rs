afaf25ead3f572c09f702648aef466f0ebee8d32
Mateusz Maćkowski <mateusz@mackowski.org>
2022-06-09 23:30:12 +0200

    fix: make mods public

--- src/lib.rs
+++ src/lib.rs
@@ -2,19 +2,18 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![allow(unused)]
 
 #[cfg(feature = "byte")]
-mod rans_byte {
+pub mod rans_byte {
     include!(concat!(env!("OUT_DIR"), "/rans_byte.rs"));
 }
 
 #[cfg(feature = "64")]
-mod rans_64 {
+pub mod rans_64 {
     include!(concat!(env!("OUT_DIR"), "/rans64.rs"));
 }
 
 #[cfg(feature = "word_sse41")]
-mod rans_word_sse41 {
+pub mod rans_word_sse41 {
     include!(concat!(env!("OUT_DIR"), "/rans_word_sse41.rs"));
 }
